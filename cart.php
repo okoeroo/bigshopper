b89076f9f819efb1955600fc6469b9d3c87f2d88
Oscar Koeroo <okoeroo@gmail.com>
2015-01-04 21:09:27 +0100

    Finished displaying cart data.

--- cart.php
+++ cart.php
@@ -73,16 +73,67 @@ function cart_get_session_products() {
     return $cart;
 }
 
+/* Get all the selected products */
 $cart = cart_get_session_products();
 if (count($cart) == 0) {
     echo '<h3>Uw winkelmandje is leeg</h3>';
 } else {
-    var_dump($cart);
-}
+    echo '<table id=cart_product_list>';
+    echo '<tr>';
+        echo '<th>Voorbeeld</th>';
+        echo '<th>SKU</th>';
+        echo '<th>Naam</th>';
+        echo '<th>Maat</th>';
+        echo '<th>Afmetingen</th>';
+        echo '<th>Prijs</th>';
+        echo '<th>Aantal</th>';
+        echo '<th>Subtotaal</th>';
+    echo '</tr>';
 
+    foreach ($cart as $cart_row) {
+        if ($cnt % 2 === 0) {
+            echo '<tr>';
+        } else {
+            echo '<tr class=cart_product_list_alt_row>';
+        }
+            echo '<td>plaatje</td>';
+            echo '<td>'.$cart_row['prod']->sku.'</td>';
+            echo '<td>'.$cart_row['prod']->name.'</td>';
+            $s = strlen($cart_row['prod']->clothing_size) > 0 ? $cart_row['prod']->clothing_size : 'nvt';
+            echo '<td>'.$s.'</td>'; /* Must change to choosen option */
+            $s = strlen($cart_row['prod']->dimensions) > 0 ? $cart_row['prod']->dimensions : 'nvt';
+            echo '<td>'.$s.'</td>'; /* Must change to choosen option */
+            echo '<td>'.
+                    number_format($cart_row['prod']->price, 2, ',', '.')
+                .' euro</td>';
+            echo '<td>'.$cart_row['count'].'</td>';
+            echo '<td>'.
+                    number_format($cart_row['prod']->price * $cart_row['count'], 2, ',', '.')
+                .' euro</td>';
+        echo '</tr>';
+    }
+
+    $total = 0.00;
+    foreach ($cart as $cart_row) {
+        $total += $cart_row['prod']->price * $cart_row['count'];
+    }
+
+        echo '<tr>'; 
+            echo '<th>Totaal</td>';
+            echo '<th>&nbsp;</td>';
+            echo '<th>&nbsp;</td>';
+            echo '<th>&nbsp;</td>';
+            echo '<th>&nbsp;</td>';
+            echo '<th>&nbsp;</td>';
+            echo '<th>&nbsp;</td>';
+            echo '<th>'.
+                    number_format($total, 2, ',', '.')
+                .' euro</td>';
+        echo '</tr>';
+    echo '</table>';
+}
 
 $tail = new Tail;
 $tail->display();
-
 
 ?>
